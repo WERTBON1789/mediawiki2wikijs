598f92c743c41f4abafd19d5e7c25c3ae02732f1
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-07-25 19:11:02 +0200

    Added reading from the environment

--- build/mediawiki2wikijs.py
+++ build/mediawiki2wikijs.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+import sys
+import os
+import paramiko
+from mediawiki_dump.dumps import LocalFileDump
+from mediawiki_dump.reader import DumpReader
+
+WIKIJS_HOST          = os.environ.get("WIKIJS_HOST")
+WIKIJS_TOKEN         = os.environ.get("WIKIJS_TOKEN")
+MEDIAWIKI_HOST       = os.environ.get("MEDIAWIKI_SSH_HOST")
+MEDIAWIKI_SSH_PORT   = os.environ.get("MEDIAWIKI_SSH_PORT")
+MEDIAWIKI_SSH_PASSWD = os.environ.get("MEDIAWIKI_SSH_PASSWD")
+MEDIAWIKI_SSH_USER   = os.environ.get("MEDIAWIKI_SSH_USER")
+
 
 def main():
     pass
