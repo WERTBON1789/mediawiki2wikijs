f8b419026b756676b5599076d3d1fa805650beba
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-08-01 15:57:14 +0200

    Removed AuthenticationUserError and used the version from wikijspy

--- build/mediawiki2wikijs.py
+++ build/mediawiki2wikijs.py
@@ -91,29 +91,6 @@ class PageCollection:
         self.metadata_list[index] = value
 
 
-class AuthenticationUserErrors(Enum):
-    Nothing = 0
-    AuthGenericError = 1001
-    AuthLoginFailed = 1002
-    AuthProviderInvalid = 1003
-    AuthAccountAlreadyExists = 1004
-    AuthTFAFailed = 1005
-    AuthTFAInvalid = 1006
-    BruteInstanceIsInvalid = 1007
-    BruteTooManyAttempts = 1008
-    UserCreationFailed = 1009
-    AuthRegistrationDisabled = 1010
-    AuthRegistrationDomainUnauthorized = 1011
-    InputInvalid = 1012
-    AuthAccountBanned = 1013
-    AuthAccountNotVerified = 1014
-    AuthValidationTokenInvalid = 1015
-    UserNotFound = 1016
-    UserDeleteForeignConstraint = 1017
-    UserDeleteProtected = 1018
-    AuthRequired = 1019
-    AuthPasswordInvalid = 1020
-
 class MediawikiMigration:
     def __init__(self, mediawiki_host: str, ssh_user: str, ssh_passwd: str, wikijs_host: str, wikijs_token: str, ssh_port=22):
         self.mediawiki_host = mediawiki_host
