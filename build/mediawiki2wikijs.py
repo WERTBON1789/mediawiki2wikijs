f9c10b175f7a63d5b589704f50b62b47722e29cd
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2023-09-19 14:43:05 +0200

    Fixed scope creep issue

--- build/mediawiki2wikijs.py
+++ build/mediawiki2wikijs.py
@@ -311,11 +311,11 @@ class MediawikiMigration:
                 self._session.execute(delete_page, {'id': page_id})
                 page_id = -1
             for index, entry in enumerate(data):
-                
+
                 # Remove all pipe characters from external links
                 split_content = entry.content.splitlines()
-                for index, line in enumerate(split_content):
-                    split_content[index] = re.sub(r'(?!.*(?<!\[)\[(?!\[).*)\s*\|\s*(?=.*(?<!\])\](?!\]))', ' ', line)
+                for idx, line in enumerate(split_content):
+                    split_content[idx] = re.sub(r'(?!.*(?<!\[)\[(?!\[).*)\s*\|\s*(?=.*(?<!\])\](?!\]))', ' ', line)
                 entry.content = '\n'.join(split_content)
 
                 exitcode, stdout, stderr = self.convert_content(entry.content)
